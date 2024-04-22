e2f2e4c18cc083b8de0bfde2b74a301eb2c742df
AlexanderContreras17 <hatakealexander24@gmail.com>
2024-04-22 12:43:16 -0600

    Creacion de vistas

---
+++ Lote_De_Autos/Views/EliminarAuto.xaml.cs
@@ -0,0 +1,9 @@
+namespace Lote_De_Autos.Views;
+
+public partial class EliminarAuto : ContentPage
+{
+	public EliminarAuto()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Lote_De_Autos/Views/VerAuto.xaml.cs
@@ -0,0 +1,9 @@
+namespace Lote_De_Autos.Views;
+
+public partial class VerAuto : ContentPage
+{
+	public VerAuto()
+	{
+		InitializeComponent();
+	}
+}
